36383daaeb8edac48f5a16bfd9d3fbf4b83ecabe
Arush Wadhawan <warush23+github@gmail.com>
2026-03-13 11:05:45 -0400

    Initial commit: Homebrew Cask for Portable VPN

---
+++ Casks/portable-vpn.rb
@@ -0,0 +1,17 @@
+cask "portable-vpn" do
+  version "1.1.0"
+  sha256 "2a75498d12c2ec90ec2c18784d4108ce2ecac92864b7794c58eb512a1ed3d2fa"
+
+  url "https://github.com/SoulSniper-V2/portable-vpn/releases/download/v#{version}/PortableVPN.dmg"
+  name "Portable VPN"
+  desc "Menu bar VPN toggle with bundled Tor"
+  homepage "https://github.com/SoulSniper-V2/portable-vpn"
+
+  app "PortableVPN.app"
+
+  zap trash: [
+    "~/.portable_tor_data",
+    "~/Library/Application Support/com.user.portablevpn",
+    "~/Library/Preferences/com.user.portablevpn.plist",
+  ]
+end
