324f4de0aa0cb1ab3695be9f883f514058d54574
Arush Wadhawan <warush23+github@gmail.com>
2026-03-30 00:18:38 -0400

    Add ClipStash cask

---
+++ Casks/clipstash.rb
@@ -0,0 +1,16 @@
+cask "clipstash" do
+  version "2.2.0"
+  sha256 "5953e1ce2cac361a3ef9956f7998b1fbe010a0e07cc3d99cc66c233452e0a72d"
+
+  url "https://github.com/SoulSniper-V2/ClipStash/releases/download/v#{version}/ClipStash-V2.dmg"
+  name "ClipStash"
+  desc "macOS menu bar clipboard manager with AI-powered search"
+  homepage "https://github.com/SoulSniper-V2/ClipStash"
+
+  app "ClipStash.app"
+
+  zap trash: [
+    "~/Library/Application Support/ClipStash",
+    "~/Library/Preferences/com.clipstash.app.plist",
+  ]
+end
