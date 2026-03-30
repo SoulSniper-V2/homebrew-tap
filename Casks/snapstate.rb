01e013d5271fcfb09f7a94c5b455413b1d17fd30
Arush Wadhawan <warush23+github@gmail.com>
2026-03-29 23:00:36 -0400

    Update snapstate cask to v1.2.1

--- Casks/snapstate.rb
+++ Casks/snapstate.rb
@@ -1,6 +1,6 @@
 cask "snapstate" do
-  version "1.0.0"
-  sha256 "f77390ba51d43db9660d88bc522b79ed0f63dd5fefe03b0796f319479d6b2286"
+  version "1.2.1"
+  sha256 "e0552700667ee38f5ae9ecf388e716c6bb190ba87da59e82ea5b17be7b248e24"
 
   url "https://github.com/SoulSniper-V2/SnapState/releases/download/v#{version}/SnapState.dmg"
   name "SnapState"
