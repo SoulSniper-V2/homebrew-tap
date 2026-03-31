5498612a9c81c15e479909d6593dd8ba5c4bf7c5
Arush Wadhawan <warush23+github@gmail.com>
2026-03-31 17:21:21 -0400

    Add FaceGuard cask

---
+++ Casks/faceguard.rb
@@ -0,0 +1,15 @@
+cask "faceguard" do
+  version "1.0.0"
+  sha256 "915389083aff5b34aad5ed396e9d5d56d99d0d0e8522209fd2024cdc2571862b"
+
+  url "https://github.com/SoulSniper-V2/FaceGuard/releases/download/v#{version}/FaceGuard-v#{version}.dmg"
+  name "FaceGuard"
+  desc "macOS menu bar app that locks your Mac when you walk away"
+  homepage "https://github.com/SoulSniper-V2/FaceGuard"
+
+  app "FaceGuard.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.soulsniper.faceguard.plist",
+  ]
+end
